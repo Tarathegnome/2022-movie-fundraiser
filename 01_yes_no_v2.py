000f0c6187850b906de1675ff920f95f5a07e0e3
Tarathegnome <163245109+Tarathegnome@users.noreply.github.com>
2024-06-10 21:52:34 +1200

    Add files via upload

---
+++ 01_yes_no_v2.py
@@ -0,0 +1,22 @@
+# functions go here
+def yes_no(question):
+
+    while True:
+        response = input(question).lower()
+
+        if response == "yes" or response == "y":
+            return "yes"
+        elif response == "no" or response == "n":
+            return "no"
+        else:
+            print("please enter yes or no")
+
+
+# main routine goes here
+while True:
+    want_instructions = yes_no("Do you want to read the instructions? ")
+
+    if want_instructions == "yes" or want_instructions == "y":
+        print("Instructions goes here")
+    else:
+        print("program continues...")
